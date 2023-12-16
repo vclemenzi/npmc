867ee9809fefad15d52716470dfa5c5d1bb81008
vclemenzi <vclemenzi@users.noreply.github.com>
2023-12-16 19:20:24 +0100

    feat: search

--- src/types/Package.ts
+++ src/types/Package.ts
@@ -16,3 +16,52 @@ export interface PackageDetailed {
     name: string;
   }[];
 }
+
+export interface PackageSearch {
+  name: string;
+  scope: string;
+  version: string;
+  description: string;
+  keywords: string[];
+  date: string;
+  links: {
+    npm: string;
+    homepage: string;
+    repository: string;
+    bugs: string;
+  };
+  author: {
+    name: string;
+    email: string;
+  };
+  publisher: {
+    username: string;
+    email: string;
+  };
+  maintainers: {
+    username: string;
+    email: string;
+  }[];
+}
+
+interface Flags {
+  insecure: number;
+}
+
+interface ScoreDetail {
+  quality: number;
+  popularity: number;
+  maintenance: number;
+}
+
+interface Score {
+  final: number;
+  detail: ScoreDetail;
+}
+
+interface PackageData {
+  package: Package;
+  flags: Flags;
+  score: Score;
+  searchScore: number;
+}
