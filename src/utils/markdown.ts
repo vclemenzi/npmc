c7600be8d16533db94a5d23b1db00a7c239585da
vclemenzi <vclemenzi@users.noreply.github.com>
2023-12-20 19:27:31 +0100

    style: moved marked initialazer to an external file

---
+++ src/utils/markdown.ts
@@ -0,0 +1,14 @@
+import { Marked } from "marked";
+import hljs from "highlight.js";
+import { markedHighlight } from "marked-highlight";
+
+export const md = new Marked(
+  markedHighlight({
+    langPrefix: "hljs language-",
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  }),
+);
+
