6c9f08074cfa967dc6acd857e0d31eb6d363c04d
vclemenzi <vclemenzi@users.noreply.github.com>
2023-12-21 15:49:24 +0100

    chore: moved to railway

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });
