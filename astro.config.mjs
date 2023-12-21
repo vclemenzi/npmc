bab3e98b00d81e28502b5285a6edf5a89c7fa597
vclemenzi <vclemenzi@users.noreply.github.com>
2023-12-21 16:30:17 +0100

    test: removed node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: "standalone"
-  })
 });
