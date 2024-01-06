a041c549adb073c0da9fecd13666c4dab3d086fb
vclemenzi <vclemenzi@users.noreply.github.com>
2024-01-06 10:27:38 +0100

    feat: general performance improvments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+import compressor from "astro-compressor";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), compressor(), sitemap()],
   adapter: node({
     mode: "standalone"
   })
