ec3cc821509c831232c40012d6c57750cf6b808c
vclemenzi <vclemenzi@users.noreply.github.com>
2024-01-10 18:44:03 +0100

    chore(deploy): vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 import compressor from "astro-compressor";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react(), compressor(), sitemap()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
