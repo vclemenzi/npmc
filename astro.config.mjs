858a4ef1e5fde2a0e57f741633ea38add770c72a
vclemenzi <vclemenzi@users.noreply.github.com>
2024-01-10 19:04:32 +0100

    chore(deploy): pages adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import compressor from "astro-compressor";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react(), compressor(), sitemap()],
-  adapter: vercel()
+  adapter: cloudflare({ mode: "directory" })
 });
